3b9d9e28e765c14f9d34e007e57f04418ca07cb4
Why <muhlutfialghifari@gmail.com>
2026-06-22 16:25:52 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,9 +43,6 @@ android {
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
     }
-    debug {
-      signingConfig = signingConfigs.getByName("debugConfig")
-    }
   }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
